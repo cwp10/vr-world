0ef28bea9cb3d4f55593c2f7091b023d6bb9fc6b
Won Pyoung Choi <cwp10@msn.com>
2018-03-03 17:45:04 +0900

    레이저 제어

--- ViveWorld/Assets/Scripts/TeleportController.cs
+++ ViveWorld/Assets/Scripts/TeleportController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportController : MonoBehaviour {
+public class TeleportController :MonoBehaviour {
 
     private SteamVR_TrackedObject trackedObj;
     private SteamVR_TrackedController trackedCtrl;
@@ -13,22 +13,37 @@ public class TeleportController : MonoBehaviour {
         }
     }
 
-    void Awake () {
+    private LineRenderer laser;
+
+    void Awake() {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
         trackedCtrl = GetComponent<SteamVR_TrackedController>();
+
+        laser = GetComponent<LineRenderer>();
+        laser.enabled = false;
     }
 
     private void OnEnable() {
         trackedCtrl.PadTouched += TrackPad;
+        trackedCtrl.PadUntouched += ReleaseTrackPad;
     }
 
     private void OnDisable() {
         trackedCtrl.PadTouched -= TrackPad;
+        trackedCtrl.PadUntouched -= ReleaseTrackPad;
     }
 
     void TrackPad(object sender, ClickedEventArgs e) {
         // 진동 발생
         controller.TriggerHapticPulse(2000);
         Debug.Log("Pad is touched");
+
+        // 레이저를 활성화
+        laser.enabled = true;
+    }
+
+    void ReleaseTrackPad(object sender, ClickedEventArgs e) {
+        // 레이저를 비활성화
+        laser.enabled = false;
     }
 }
