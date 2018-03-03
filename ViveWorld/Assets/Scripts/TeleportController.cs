049e26d4c05c4be43d07f5658f6344757a5a9852
Won Pyoung Choi <cwp10@msn.com>
2018-03-03 17:40:54 +0900

    텔레포트 컨트롤러 진동 처리

---
+++ ViveWorld/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_TrackedController trackedCtrl;
+
+    private SteamVR_Controller.Device controller {
+        get {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
+
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        trackedCtrl = GetComponent<SteamVR_TrackedController>();
+    }
+
+    private void OnEnable() {
+        trackedCtrl.PadTouched += TrackPad;
+    }
+
+    private void OnDisable() {
+        trackedCtrl.PadTouched -= TrackPad;
+    }
+
+    void TrackPad(object sender, ClickedEventArgs e) {
+        // 진동 발생
+        controller.TriggerHapticPulse(2000);
+        Debug.Log("Pad is touched");
+    }
+}
