d9d11169b4be21ed12ffd15511694043ac25a290
Won Pyoung Choi <cwp10@msn.com>
2018-03-03 16:30:13 +0900

    컨트롤러 이벤트 테스트

---
+++ ViveWorld/Assets/Scripts/ViveController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveController : MonoBehaviour {
+  
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device controller {
+        get {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
+
+	void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+
+	void Update () {
+        if (controller == null) {
+            Debug.Log("Controller is not detected");
+            return;
+        }
+
+        // 트리거  버튼 클릭
+        if (controller.GetHairTriggerDown()) {
+            Debug.Log("Trigger " + controller.index + " is pressed");
+        }
+
+        // 트리거 버튼 릴리즈
+        if (controller.GetHairTriggerUp()) {
+            Debug.Log("Trigger " + controller.index + " is unpressed");
+        }
+
+        // 메뉴 버튼 클릭
+        if (controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu)) {
+            Debug.Log(controller.index + " App Button pressed");
+        }
+
+        // 메뉴 버튼 릴리즈
+        if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip)) {
+            Debug.Log(controller.index + " App Button unpressed");
+        }
+
+        // 트랙패드 터치 여부와 좌표 산출
+        if (controller.GetTouch(SteamVR_Controller.ButtonMask.Touchpad)) {
+            Vector2 pad = controller.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
+            Debug.Log("TouchPad = " + controller.index + " " + pad);
+        }
+    }
+}
